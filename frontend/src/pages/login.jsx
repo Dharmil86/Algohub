8d42d915ccc82d0ab707939b3f9a5dc570c75c48
Dharmil86 <dharmilparmar86@gmail.com>
2025-08-19 23:22:47 +0530

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch(`{import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
