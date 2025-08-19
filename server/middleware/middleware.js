1829d39d025a8b1c6d31c7d90d690232aba50145
Dharmil86 <dharmilparmar86@gmail.com>
2025-08-19 23:44:44 +0530

    Update middleware.js

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -12,7 +12,7 @@ const authenticateUser = async (req, res, next) => {
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET || "mysecret");
 
-    const user = await User.findById(decoded.id).select("-password");
+    const user = await User.findById(decoded.id || decoded._id).select("-password");
     if (!user) {
       return res.status(404).json({ success: false, message: "User not found" });
     }
